4782091dafcbc833dee458d10e896ae576a82cd2
Lalita Desouza <lmdesou@yahoo.com>
2017-10-23 14:27:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var uppercase = "HELLO"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello'
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'I love you, Grandma.'
+
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO")
+}
+function logWhisper(string) {
+  console.log("hello")
+}
+function sayHiToGrandma(string) {
+  return ("I can't hear you!")
+}
+function sayHiToGrandma(string) {
+  return ("YES INDEED!")
+}
