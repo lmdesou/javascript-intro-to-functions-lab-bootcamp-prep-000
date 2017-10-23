047a23fd4d19aff6dd76aef7083847ae20e00989
Lalita Desouza <lmdesou@yahoo.com>
2017-10-23 15:02:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
-    console.log ("YES INDEED!")
-  } else if (string === string.toLowerCase())
-  return ("I can\'t hear you!")
+    return ("YES INDEED!")
+  } else if (string === string.toLowerCase()) {
+    return ("I can\'t hear you!")
+  }
 }
