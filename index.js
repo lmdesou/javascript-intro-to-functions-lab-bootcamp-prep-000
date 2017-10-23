3b33bfbee328239b32a39bfe3c41a35d7330d73a
Lalita Desouza <lmdesou@yahoo.com>
2017-10-23 15:07:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log()
 }
 function logWhisper(string) {
   console.log(string.toLowerCase())
