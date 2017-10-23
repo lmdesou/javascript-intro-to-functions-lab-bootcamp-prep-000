38d2db94ffc1f64487bbd6b12c732d08740fb578
Lalita Desouza <lmdesou@yahoo.com>
2017-10-23 15:07:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var uppercase = "HELLO"
-
-uppercase.toUpperCase() === uppercase
-
-var lowercase = 'hello'
-
-lowercase.toLowerCase() === lowercase
-
-var mixedCase = 'I love you, Grandma.'
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -15,16 +5,17 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log("HELLO")
+  console.log(string.toUpperCase())
 }
 function logWhisper(string) {
-  console.log("hello")
+  console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return ("YES INDEED!")
   } else if (string === string.toLowerCase()) {
     return ("I can\'t hear you!")
-  } else if (string === "I love you, Grandma.")
-  return ("I love you, too.")
+  } else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
 }
