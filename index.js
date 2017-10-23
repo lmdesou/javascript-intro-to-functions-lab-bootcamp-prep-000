045ea40fc8952e4aed028432cbe9ec1a081fca74
Lalita Desouza <lmdesou@yahoo.com>
2017-10-23 15:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function logShout(string) {
   console.log(shout(string))
 }
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(whisper(string))
 }
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
